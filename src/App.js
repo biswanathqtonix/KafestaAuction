87f58b20da75667ba345fd4ab112054e4c419623
Biswanath Prasad Singh <biswanathprasadsingh9@gmail.com>
2020-12-09 05:14:55 +0530

    my

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Test
+          Learn React JS
         </a>
       </header>
     </div>
