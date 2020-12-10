5a67db3a25b1b230f7310b5b5bd3ceda643124c7
Biswanath Prasad Singh <biswanathprasadsingh9@gmail.com>
2020-12-10 07:37:06 +0530

    'chnages10122020'

--- src/includes/LoadAllJS.js
+++ src/includes/LoadAllJS.js
@@ -9,24 +9,25 @@ export default class LoadAllJS extends Component {
         loadjs('https://code.jquery.com/jquery-migrate-1.4.1.min.js', () => {});
         loadjs('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', () => {});
         loadjs('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', () => {});
-        loadjs('http://localhost:3000/assets/plugins/switcher/js/dmss.js', () => {});
-        loadjs('http://localhost:3000/assets/libs/bootstrap-select.min.js', () => {});
-        loadjs('http://localhost:3000/assets/plugins/magnific-popup/jquery.magnific-popup.min.js', () => {});
-        loadjs('http://localhost:3000/assets/plugins/headers/slidebar.js', () => {});
-        loadjs('http://localhost:3000/assets/plugins/headers/header.js', () => {});
-        loadjs('http://localhost:3000/assets/plugins/jqBootstrapValidation.js', () => {});
-        loadjs('http://localhost:3000/assets/plugins/contact_me.js', () => {});
-        loadjs('http://localhost:3000/assets/plugins/flowplayer/flowplayer.min.js', () => {});
-        loadjs('http://localhost:3000/assets/plugins/isotope/isotope.pkgd.min.js', () => {});
-        loadjs('http://localhost:3000/assets/plugins/isotope/imagesLoaded.js', () => {});
-        loadjs('http://localhost:3000/assets/plugins/rendro-easy-pie-chart/jquery.easypiechart.min.js', () => {});
-        loadjs('http://localhost:3000/assets/plugins/rendro-easy-pie-chart/jquery.waypoints.min.js', () => {});
-        loadjs('http://localhost:3000/assets/plugins/scrollreveal/scrollreveal.min.js', () => {});
-        loadjs('http://localhost:3000/assets/plugins/ofi.min.js', () => {});
-        loadjs('http://localhost:3000/assets/plugins/slider-pro/jquery.sliderPro.min.js', () => {});
-        loadjs('http://localhost:3000/assets/plugins/slick/slick.js', () => {});
-        loadjs('http://localhost:3000/assets/js/custom.js', () => {});
-      }
+        loadjs(process.env.REACT_APP_APP_URL+'assets/plugins/switcher/js/dmss.js', () => {});
+        loadjs(process.env.REACT_APP_APP_URL+'assets/libs/bootstrap-select.min.js', () => {});
+        loadjs(process.env.REACT_APP_APP_URL+'assets/plugins/magnific-popup/jquery.magnific-popup.min.js', () => {});
+        loadjs(process.env.REACT_APP_APP_URL+'assets/plugins/headers/slidebar.js', () => {});
+        loadjs(process.env.REACT_APP_APP_URL+'assets/plugins/headers/header.js', () => {});
+        loadjs(process.env.REACT_APP_APP_URL+'assets/plugins/jqBootstrapValidation.js', () => {});
+        loadjs(process.env.REACT_APP_APP_URL+'assets/plugins/contact_me.js', () => {});
+        loadjs(process.env.REACT_APP_APP_URL+'assets/plugins/flowplayer/flowplayer.min.js', () => {});
+        loadjs(process.env.REACT_APP_APP_URL+'assets/plugins/isotope/isotope.pkgd.min.js', () => {});
+        loadjs(process.env.REACT_APP_APP_URL+'assets/plugins/isotope/imagesLoaded.js', () => {});
+        loadjs(process.env.REACT_APP_APP_URL+'assets/plugins/rendro-easy-pie-chart/jquery.easypiechart.min.js', () => {});
+        loadjs(process.env.REACT_APP_APP_URL+'assets/plugins/rendro-easy-pie-chart/jquery.waypoints.min.js', () => {});
+        loadjs(process.env.REACT_APP_APP_URL+'assets/plugins/scrollreveal/scrollreveal.min.js', () => {});
+        loadjs(process.env.REACT_APP_APP_URL+'assets/plugins/ofi.min.js', () => {});
+        loadjs(process.env.REACT_APP_APP_URL+'assets/plugins/slider-pro/jquery.sliderPro.min.js', () => {});
+        loadjs(process.env.REACT_APP_APP_URL+'assets/plugins/slick/slick.js', () => {});
+        loadjs(process.env.REACT_APP_APP_URL+'assets/js/custom.js', () => {});
+        window.scrollTo(0,0);
+    }
 
     render() {
         return (
